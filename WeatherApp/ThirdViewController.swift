d5530006ecfc7483ecc2357a97baf3522d636275
Taavi Tuomela <c6tatuom@kc408mac06.tamk.ad>
2018-10-03 12:01:00 +0300

    Created level

---
+++ WeatherApp/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  WeatherApp
+//
+//  Created by Taavi Tuomela on 03/10/2018.
+//  Copyright © 2018 Taavi Tuomela. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
