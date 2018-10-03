638c5bafbc3f1a9a05e5e46a142fd81e556024a8
Taavi Tuomela <c6tatuom@kc408mac06.tamk.ad>
2018-10-03 11:32:14 +0300

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Taavi Tuomela on 03/10/2018.
+//  Copyright © 2018 Taavi Tuomela. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Taavi Tuomela on 03/10/2018.
+//  Copyright © 2018 Taavi Tuomela. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
